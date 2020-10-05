56c3ab87138592c423c1e7288cb14362ad0e334c
frwu93 <franklinw888@gmail.com>
2020-10-05 16:59:58 -0400

    temporarily done with Cell, ConwayCell, and GameBoard

---
+++ src/cellsociety/model/ConwayGameOfLife.java
@@ -0,0 +1,14 @@
+package cellsociety.model;
+
+public class ConwayGameOfLife {
+
+  private GameBoard board;
+  private int generation;
+
+  public ConwayGameOfLife(GameBoard board){
+    this.board = board;
+  }
+
+
+
+}
