99a96984103cc7218a9e21f257d148ba9ec8167b
Josh Boss <jsb91@duke.edu>
2020-10-05 01:39:25 -0400

    fixed magic values

--- src/cellsociety/view/saveFiles.java
+++ src/cellsociety/view/saveFiles.java
@@ -6,10 +6,17 @@ import java.io.IOException;
 
 public class saveFiles {
 
-    public void saveState(String[][] currentState) throws IOException {
-        int directoryLength = new File("resources/Game_CSVs/").list().length;
+    private static final String SAVE_DIR = "resources/Game_CSVs/";
+    private static final String NEW = "new";
+    private static final String EXTENSION = ".csv";
+    private static final String COMMA_DELIM = ",";
+    private static final String NEW_LINE = "\n";
 
-        FileWriter csvWriter = new FileWriter("resources/Game_CSVs/new"+ directoryLength +".csv");
+
+    public void saveState(String[][] currentState) throws IOException {
+        int directoryLength = new File(SAVE_DIR).list().length;
+
+        FileWriter csvWriter = new FileWriter(SAVE_DIR+ NEW+ directoryLength +EXTENSION);
         makeHeader(csvWriter,currentState);
         writeRows(csvWriter,currentState);
         csvWriter.flush();
@@ -19,21 +26,21 @@ public class saveFiles {
 
     private void makeHeader(FileWriter csvWriter, String[][] currentState) throws IOException {
         csvWriter.append(Integer.toString(currentState.length));
-        csvWriter.append(",");
+        csvWriter.append(COMMA_DELIM);
         csvWriter.append(Integer.toString(currentState[0].length));
-        csvWriter.append("\n");
+        csvWriter.append(NEW_LINE);
     }
 
     private void writeRows(FileWriter csvWriter, String[][] currentState) throws IOException {
         for(int row =0; row< currentState.length; row++){
             for(int column = 0; column <currentState[row].length; column++){
                 if (column < currentState[row].length-1) {
-                    csvWriter.append(currentState[row][column] + ",");
+                    csvWriter.append(currentState[row][column] + COMMA_DELIM);
                 } else {
                     csvWriter.append(currentState[row][column]);
                 }
             }
-            csvWriter.append("\n");
+            csvWriter.append(NEW_LINE);
         }
 
     }
