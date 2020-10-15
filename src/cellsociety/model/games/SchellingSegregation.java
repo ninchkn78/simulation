05e215624e66c58ffc4c4f7e501dfddfe34d38f8
frwu93 <franklinw888@gmail.com>
2020-10-14 22:52:16 -0400

    working on schelling segregation

--- src/cellsociety/model/games/SchellingSegregation.java
+++ src/cellsociety/model/games/SchellingSegregation.java
@@ -1,5 +1,40 @@
 package cellsociety.model.games;
 
+import cellsociety.model.GameBoard;
+import cellsociety.model.cells.SpreadingFireCell;
+
 public class SchellingSegregation extends Simulation{
+
+  public SchellingSegregation(String csvConfig){
+    super(csvConfig);
+  }
+
+
+  @Override
+  public void updateCell(GameBoard gameBoard, int row, int col) {
+
+  }
+
+  public int countNeighbors(int row, int col){
+    int neighborCount = 0;
+    for (int i = row - 1; i <= row + 1; i++){
+      for (int j = col - 1; j <= col + 1; j++){
+        if (getGameBoard().inBounds(i,j)){
+          neighborCount++;
+        }
+      }
+    }
+    return neighborCount;
+  }
+
+  public boolean isDifferent(int x, int y, int row, int col) { //TODO: Move to cell
+    return !getState(x, y).equals(getState(row, col));
+  }
+
+  public String getState(int row, int col){
+    return getGameBoard().getCell(row, col).getState();
+  }
+
+
 
 }
