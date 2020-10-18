4551139aae2a088666494581c299fdd1ac90e9ac
Josh Boss <jsb91@duke.edu>
2020-10-18 15:22:41 -0400

    finished button reflection!

--- src/cellsociety/view/ButtonSetup.java
+++ src/cellsociety/view/ButtonSetup.java
@@ -3,6 +3,8 @@ package cellsociety.view;
 import cellsociety.controller.Controller;
 import cellsociety.model.GameBoard;
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.event.ActionEvent;
@@ -28,15 +30,23 @@ public class ButtonSetup {
   public void buttonPipeline(List<String> buttonNames, Group root){
       List<Button> buttonList = new ArrayList<>();
       for(String buttonName: buttonNames){
-
+          //TODO - get the button name from the text resources file
           Button currentButton = new Button(buttonName);
-          currentButton.setId("buttonName");
+          currentButton.setId(buttonName);
           buttonList.add(currentButton);
           myHbox.getChildren().add(currentButton);
-          //TODO 10/18- add something here I think with a lambda function maybe? Or Reflection? That checks all of the button status'
+          System.out.println(buttonName);
+          try {
+            Method method = this.getClass().getDeclaredMethod("check" + buttonName, Button.class);
+            method.invoke(this, currentButton);
+          }
+          catch(SecurityException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e){
+            e.printStackTrace();
+          }
+
+        //TODO 10/18- add something here I think with a lambda function maybe? Or Reflection? That checks all of the button status'
         }
         root.getChildren().add(myHbox);
-        checkButtonStatus(buttonList);
   }
 
   private HBox createHBox() {
@@ -48,17 +58,7 @@ public class ButtonSetup {
   }
 
 
-  public void checkButtonStatus(List<Button> buttonList) {
-    //TODO - this method will be deleted once above todo is taken care of. not exactly sure how to do it yet
-    checkFileWriteButton(buttonList.get(3));
-    checkFileReaderButton(buttonList.get(0));
-    checkRunButton(buttonList.get(1));
-    checkPauseButton(buttonList.get(2));
-    checkImageButton(buttonList.get(4));
-    checkStepButton(buttonList.get(5));
-  }
-
-  public void checkStepButton(Button stepButton) {
+  public void checkStepOnce(Button stepButton) {
     stepButton.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent e) {
@@ -67,10 +67,9 @@ public class ButtonSetup {
       }
     });
 
-
   }
 
-  public void checkFileReaderButton(Button loadFileButton) {
+  public void checkLoadFile(Button loadFileButton) {
     loadFileButton.setOnAction(e -> {
       fileSelected = true;
       myDisplay.pauseGame();
@@ -84,7 +83,7 @@ public class ButtonSetup {
     });
   }
 
-  public void checkRunButton(Button runButton) {
+  public void checkPlay(Button runButton) {
     runButton.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent e) {
@@ -94,7 +93,7 @@ public class ButtonSetup {
     });
   }
 
-  public void checkPauseButton(Button pauseButton) {
+  public void checkPause(Button pauseButton) {
     pauseButton.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent e) {
@@ -105,7 +104,7 @@ public class ButtonSetup {
     });
   }
 
-  public void checkFileWriteButton(Button fileSaveButton) {
+  public void checkSaveFile(Button fileSaveButton) {
     fileSaveButton.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent e) {
@@ -121,7 +120,7 @@ public class ButtonSetup {
     });
   }
 
-  public void checkImageButton(Button chooseImageButton) {
+  public void checkImage(Button chooseImageButton) {
     chooseImageButton.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent e) {
--- src/cellsociety/view/Display.java
+++ src/cellsociety/view/Display.java
@@ -98,7 +98,7 @@ public class Display extends Application {
 
   public void parseButtonsFromProperties(){
     List<String> buttonNameList = new ArrayList<>();
-    for(int buttonNum = 1; buttonNum<=NUMBER_POSSIBLE_BUTTONS; buttonNum++){
+    for(int buttonNum = 1; buttonNum<=6; buttonNum++){
       if(!((String)myController.getProperties().get("Button"+buttonNum)).equals("")) {
         buttonNameList.add((String) myController.getProperties().get("Button" + buttonNum));
       }
